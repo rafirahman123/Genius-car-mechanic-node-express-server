19d80091c2789d3f576ec89da5b78fcf9277b175
rafirahman123 <76124938+rafirahman123@users.noreply.github.com>
2021-10-27 19:46:44 +0600

    console change

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('server running');
+    res.send('genius server running');
 });
 
 app.listen(port, () => {
